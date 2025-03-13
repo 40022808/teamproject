b27f7c0f9d584da091edbe024da3d521ec5918b0
Dargai Dániel <dargaidanielferenc@ktch.hu>
2025-03-13 22:22:22 +0100

    MEGVAN

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -9,22 +9,33 @@ class BookingController extends Controller
     public function checkBooking(Request $request)
     {
         $date = $request->query('date');
-        $booked = Booking::where('date', $date)->exists();
-        return response()->json(['booked' => $booked]);
+        $booked = Booking::where('date', $date)->exists(); 
+
+        return response()->json(['booked' => $booked]); 
     }
 
     public function storeBooking(Request $request)
     {
+        
         $date = $request->input('date');
+
+        if(!$date) {
+            return response()->json(['error'=> 'Date is required'], 400);
+        }
+        $dateTime = new \DateTime($date);
+        $date = $dateTime->format('Y-m-d');
+
         $booking = new Booking();
         $booking->date = $date;
         $booking->save();
-        return response()->json(['success' => true]);
+
+        return response()->json(['success' => true, 'date' => $date]);
     }
 
     public function getBookedDates()
     {
-        $dates = Booking::pluck('date');
-        return response()->json(['dates' => $dates]);
+        $dates = Booking::pluck('date'); // Lekérjük az összes foglalt dátumot
+
+        return response()->json(['dates' => $dates]); // Helyes változónév
     }
 }
--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Booking;
+use Illuminate\Http\Request;
+
 
 class Booking extends Model
 {
